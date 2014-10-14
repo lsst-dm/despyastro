70947f76ab59a228e9181fab70c6a29ad1fce9f2
Felipe Menanteau <felipe@illinois.edu>
2014-10-14 21:50:59 +0000

    added genutils to despyasrtro

--- python/despyastro/__init__.py
+++ python/despyastro/__init__.py
@@ -1,9 +1,9 @@
 __author__  = "Felipe Menanteau"
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 version = __version__
 
 """
- A collection of Astro-relate Python functions useful for DESDM
+ A collection of Astro-related Python functions useful for DESDM
  Python-based modules
 """
 
@@ -11,4 +11,4 @@ from . import astrometry
 from . import coords
 from . import tableio
 from . import wcsutil
-
+from . import genutil
---
+++ python/despyastro/genutil.py
@@ -0,0 +1,36 @@
+"""
+A set of general (for lack of better word) python utilities functions
+that use numpy natively.
+Felipe Menanteau, NCSA Oct 2014
+"""
+
+def query2dict_of_columns(query,dbhandle,array=False):
+
+    """
+    Transforms the result of an SQL query and a Database handle object [dhandle]
+    into a dictionary of list or numpy arrays if array=True
+    """ 
+
+    if array: import numpy
+    # Get the cursor from the DB handle
+    cur = dbhandle.cursor()
+    # Execute
+    cur.execute(query)
+    # Get them all at once
+    list_of_tuples = cur.fetchall()
+    # Get the description of the columns to make the dictionary
+    desc = [d[0] for d in cur.description] 
+
+    querydic = {} # We will populate this one
+    cols = zip(*list_of_tuples)
+    for k in range(len(cols)):
+        key = desc[k]
+        if array:
+            if isinstance(cols[k][0],str):
+                querydic[key] = numpy.array(cols[k],dtype=object)
+            else:
+                querydic[key] = numpy.array(cols[k])
+        else:
+            querydic[key] = cols[k]    
+    return querydic 
+
