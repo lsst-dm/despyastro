5cf779ece316465adf624d768c1eef0547d4a640
Felipe Menanteau <felipe@illinois.edu>
2014-12-18 17:06:10 +0000

    now from loading all of genutil at __init__ time

--- python/despyastro/__init__.py
+++ python/despyastro/__init__.py
@@ -1,5 +1,5 @@
 __author__  = "Felipe Menanteau"
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 version = __version__
 
 """
@@ -12,3 +12,4 @@ from . import coords
 from . import tableio
 from . import wcsutil
 from . import genutil
+from .genutil import *
