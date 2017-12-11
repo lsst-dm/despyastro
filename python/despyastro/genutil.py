b5080842e4d43f0a020b185a678cd7f92a43e3e2
Mikolaj Kowalik <mxk@illinois.edu>
2017-12-11 12:53:16 -0600

    Revert "Merge branch 'tickets/DM-12247'"
    This reverts commit 68e021ad994e2ff5bd3d47271eadd675cd3582d4, reversing
    changes made to 868cac8b228691b5f603db99b75fcc745cdcbbf5 which means
    master branch holds Python 2 version (tagged as 1.01) again.

--- python/despyastro/genutil.py
+++ python/despyastro/genutil.py
@@ -1,17 +1,18 @@
-"""A set of general  python utilities functions that use numpy natively.
+"""
+A set of general (for lack of better word) python utilities functions
+that use numpy natively.
+Felipe Menanteau, NCSA Oct 2014
 """
 import collections
 
+def query2dict_of_columns(query,dbhandle,array=False):
 
-def query2dict_of_columns(query, dbhandle, array=False):
-    """Transforms the result of an SQL query and a Database handle.
-
+    """
     Transforms the result of an SQL query and a Database handle object [dhandle]
     into a dictionary of list or numpy arrays if array=True
-    """
+    """ 
 
-    if array:
-        import numpy
+    if array: import numpy
     # Get the cursor from the DB handle
     cur = dbhandle.cursor()
     # Execute
@@ -19,25 +20,26 @@ def query2dict_of_columns(query, dbhandle, array=False):
     # Get them all at once
     list_of_tuples = cur.fetchall()
     # Get the description of the columns to make the dictionary
-    desc = [d[0] for d in cur.description]
+    desc = [d[0] for d in cur.description] 
 
     querydic = collections.OrderedDict() # We will populate this one
-    cols = list(zip(*list_of_tuples))
+    cols = zip(*list_of_tuples)
     for k in range(len(cols)):
         key = desc[k]
         if array:
-            if isinstance(cols[k][0], str):
-                querydic[key] = numpy.array(cols[k], dtype=object)
+            if isinstance(cols[k][0],str):
+                querydic[key] = numpy.array(cols[k],dtype=object)
             else:
                 querydic[key] = numpy.array(cols[k])
         else:
-            querydic[key] = cols[k]
-    return querydic
+            querydic[key] = cols[k]    
+    return querydic 
 
-
-def query2rec(query, dbhandle, verb=False):
-    """Queries DB and returns results as a numpy recarray.
+def query2rec(query,dbhandle,verb=False):
     """
+    Queries DB and returns results as a numpy recarray.
+    """ 
+
     import numpy
     # Get the cursor from the DB handle
     cur = dbhandle.cursor()
@@ -45,11 +47,10 @@ def query2rec(query, dbhandle, verb=False):
     cur.execute(query)
     tuples = cur.fetchall()
 
-    # Return rec array
-    if len(tuples) > 0:
-        names = [d[0] for d in cur.description]
-        return numpy.rec.array(tuples, names=names)
+    # Return rec array                                                                                                                                          
+    if len(tuples)>0:
+        names  = [d[0] for d in cur.description]
+        return numpy.rec.array(tuples,names=names)
     else:
-        if verb:
-            print("# WARNING DB Query in query2rec() returned no results")
-        return False
+        if verb: print "# WARNING DB Query in query2rec() returned no results"
+	return False
